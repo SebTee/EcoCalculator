916aa5137d53e3c5eb2883aa914195ffe293b1d3
sebastian <seb.tom.tee@gmail.com>
2020-02-10 16:14:32 +0000

    Created database creation file

---
+++ database/createDB.sql
@@ -0,0 +1,29 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE user(
+user_id SERIAL PRIMARY KEY,
+user_name VARCHAR(32) NOT NULL,
+user_password VARCHAR(256) NOT NULL,
+user_email VARCHAR(345) NOT NULL UNIQUE
+);
+
+CREATE TABLE question(
+question_id SERIAL PRIMARY KEY,
+question VARCHAR(128) NOT NULL UNIQUE
+);
+
+CREATE TABLE answer(
+answer_id SERIAL PRIMARY KEY,
+answer_value INT NOT NULL,
+answer_display VARCHAR(16) NOT NULL,
+question_id INT REFERENCES question(question_id)
+);
+
+CREATE TABLE result(
+user_id INT REFERENCES user(user_id),
+question_id INT REFERENCES question(question_id),
+answer_id INT REFERENCES answer(answer_id),
+PRIMARY KEY(user_id, question_id)
+);
+
+END TRANSACTION;
