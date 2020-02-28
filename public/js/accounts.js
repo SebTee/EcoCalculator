8625074a651e3e7a3f8bbedbae06695a7b9e653b
Olliejb1 <up896129@myport.ac.uk>
2020-02-28 17:03:10 +0000

    Created js for account creation frontend using api call

---
+++ public/js/accounts.js
@@ -0,0 +1,21 @@
+function createAccount(){
+    const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const pwConfirm = document.getElementById("pwConfirm").value;
+
+    if (pwConfirm === password){
+        fetch('/api/v1/account/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            })
+        }).then(res => console.log(res))
+    }else{
+        alert("Passwords don't match")
+    }
+}
