e110450037ca9b0691b9c62a1e2ca14947a4e5de
sebastian <seb.tom.tee@gmail.com>
2020-03-06 15:39:19 +0000

    Added JSDoc documentation to result.js

--- result.js
+++ result.js
@@ -2,6 +2,11 @@ const pool = require("./database/dbClient");
 const getQuestionsAsJson = require("./question").getJson;
 const isLoggedIn = require("./account").isLoggedIn;
 
+/**
+ * Store a user's answer in the database.
+ * @param {number} accountId User's account ID.
+ * @param {Array.object} answers Array of answers.
+ */
 async function storeResults(accountId, answers) {
 	const client = await pool.connect();
 	let error = undefined;
@@ -23,6 +28,15 @@ async function storeResults(accountId, answers) {
 	if (error !== undefined) throw error;
 }
 
+/**
+ * <p>Handles the get saved results API call.</p>
+ * <p>200 response with the user's saved results returned in the body.</p>
+ * <p>401 response if the user is not logged in.</p>
+ * <p>404 response If the user has not got any answer's stored.</p>
+ * <p>500 response if an unknown error occurs.</p>
+ * @param {object} req API request object
+ * @param {object} res API response object
+ */
 function getResults(req, res) {
 	if (isLoggedIn(req)) { //checks is user logged in
 		pool.query('SELECT question_id, answer_id FROM account_answer WHERE $1', [req.session.sessionName])
@@ -49,6 +63,14 @@ function getResults(req, res) {
 	}
 }
 
+/**
+ * <p>Handles the get submit results API call. If the user is logged in the user's results are saved in the database.</p>
+ * <p>200 response with the user's results returned in the body.</p>
+ * <p>400 response If the request body is in an invalid format.</p>
+ * <p>500 response if an unknown error occurs.</p>
+ * @param {object} req API request object
+ * @param {object} res API response object
+ */
 function submit(req, res) {
 	isBodyValid(req.body.answers).then(valid => {
 		if (valid !== undefined) {
@@ -70,6 +92,13 @@ function submit(req, res) {
 	});
 }
 
+/**
+ * Checks if the body of a request is valid.
+ * @param {Array.object} answers Array of answer objects.
+ * @param {number} answers[].questionId ID of the question answered.
+ * @param {number} answers[].answerId ID of the answer.
+ * @returns {boolean} True if the the answer's are valid otherwise returns false.
+ */
 async function isBodyValid(answers) {
 	if (answers === undefined) return false;
 	const questionsJson = await getQuestionsAsJson();
@@ -81,6 +110,20 @@ async function isBodyValid(answers) {
 	return true;
 }
 
+/**
+ * Checks if the answer to a question is valid.
+ * @param {Array.object} answers Array of answer objects.
+ * @param {number} answers[].questionId ID of the question answered.
+ * @param {number} answers[].answerId ID of the answer.
+ * @param {object} question Question object which contains all valid answers
+ * @param {number} question.questionId - Question's ID.
+ * @param {string} question.question - Question string to be displayed to a user.
+ * @param {Array.object} question.answers - Array of possible answers.
+ * @param {number} question.answers[].answerId - Answer's id.
+ * @param {string} question.answers[].answer - Answer string to be displayed to a user.
+ * @param {number} question.answers[].value - Answer's score.
+ * @returns {boolean} True if the the answer to the given question is valid otherwise returns false.
+ */
 function isQuestionAnswered(question, answers) {
 	for (let userAnswerIndex = 0; userAnswerIndex < answers.length; userAnswerIndex++) {
 		if (question.questionId === answers[userAnswerIndex].questionId) {
@@ -95,6 +138,18 @@ function isQuestionAnswered(question, answers) {
 	return false;
 }
 
+
+/**
+ * Generates results to be sent back to the user.
+ * @param {Array.object} answers Array of answer objects.
+ * @param {number} answers[].questionId ID of the question answered.
+ * @param {number} answers[].answerId ID of the answer.
+ * @property {object} response - Response to sent back.
+ * @property {number} response.score - User's ecology score.
+ * @property {Array.string} response.suggestions - List of suggestion's to reduce climate impact.
+ * @property {Array.object} response.categoryValues - List of values associated for categories.
+ * @return {object} response
+ */
 async function generateResults(answers) {
 	let response = {
 		"score": 0,
