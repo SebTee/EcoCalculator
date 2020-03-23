4f9b0f188d7889f92011951864d3fdceabbe7dc5
sebastian <seb.tom.tee@gmail.com>
2020-03-23 14:46:39 +0000

    Added documentation for express object and routers

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
+/**
+ * Route to the index page
+ * @type {Router}
+ */
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
